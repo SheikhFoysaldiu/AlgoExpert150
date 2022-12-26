4b4ec014705fa177897c6a94856cab1793b79b01
unknown <foysal15-3442@diu.edu.bd>
2022-12-26 21:10:33 +0600

    Suffix Trie

---
+++ Suffix Trie/SuffixTrie.py
@@ -0,0 +1,54 @@
+class SuffixTrie:
+    def __init__(self,string):
+        self.root = {}
+        self.endSymbol = "*"
+        self.populateSuffixTrieFrom(string)
+
+    # O(n^2) time | O(n^2) space
+    def populateSuffixTrieFrom(self,string):
+        for i in range(len(string)):
+            self.insertSubstringStartingAt(i,string)
+    
+    # O(m) time | O(1) space
+    def insertSubstringStartingAt(self,i,string):
+        node = self.root
+        for j in range(i,len(string)):
+            letter = string[j]
+            if letter not in node:
+                node[letter] = {}
+            node = node[letter]
+        
+        node[self.endSymbol] = True
+    # O(m) time | O(1) space
+    def contains(self,string):
+        node = self.root
+        for letter in string:
+            if letter not in node:
+                return False
+            node = node[letter]
+        return self.endSymbol in node
+
+
+# Time Complexity: O(n^2 + m) where n is the length of the input string and m is the length of the query string.
+# Space Complexity: O(n^2) where n is the length of the input string.
+# Test
+#                                  Root
+#                               /   |    \
+#                              b    a     c
+#                             / \  / \   / \
+#                            a   c b   c a   *
+#                           / \   |   |   \
+#                          c   *  *   *   *
+#                         / \
+#                        *   *
+
+string = "babc"
+suffixTrie = SuffixTrie(string)
+print(suffixTrie.contains("abc")) # True
+print(suffixTrie.contains("ab")) # False
+print(suffixTrie.contains("babc")) # True
+print(suffixTrie.contains("bc")) # True
+print(suffixTrie.contains("c")) # True
+print(suffixTrie.contains("b")) # False
+print(suffixTrie.contains("ba")) # False
+print(suffixTrie.contains("bab")) # False
